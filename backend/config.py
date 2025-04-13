2df93c4c43e12c9f2ad948306ef949ebe191ced6
Sachi Singh <sachisingh2005@gmail.com>
2025-04-13 16:32:56 +0530

    Update config.py
    safety update

--- backend/config.py
+++ backend/config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "secure_password_2025")
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secure_password")
     MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
     MYSQL_USER = os.getenv("MYSQL_USER", "root")
-    MYSQL_PASSWORD = urllib.parse.quote_plus(os.getenv("MYSQL_PASSWORD", "Sachi@2005"))
+    MYSQL_PASSWORD = urllib.parse.quote_plus(os.getenv("MYSQL_PASSWORD", "MYSQL_PASSWORD"))
     MYSQL_DB = os.getenv("MYSQL_DB", "mental_health_tracker")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
     )
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "secure_password_2025")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_secure_password")
